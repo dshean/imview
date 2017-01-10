3b43d9f29de0e3c88b44a8d8fa77b698533d2e07
David Shean <dshean@gmail.com>
2017-01-09 23:30:32 -0800

    review_img: update

--- imview/review_img.py
+++ imview/review_img.py
@@ -1,13 +1,11 @@
 #! /usr/bin/env python
-
-#David Shean
-#dshean@gmail.com
-
-#Simple interactive image viewer to generate good/bad lists
-#Plotting works in ipython
+"""
+Simple interactive image viewer to generate good/bad lists from input list of filenames
+"""
 
 import sys
 import os
+import argparse
 
 from osgeo import gdal
 import matplotlib.pyplot as plt
@@ -16,7 +14,7 @@ import matplotlib.image as mpimg
 from pygeotools.lib import iolib
 from pygeotools.lib import malib
 
-from . import gmtColormap
+from imview.lib import gmtColormap
 cpt_rainbow = gmtColormap.get_rainbow()
 plt.register_cmap(cmap=cpt_rainbow)
 
@@ -52,75 +50,89 @@ def query_yes_no(question, default="yes"):
             sys.stdout.write("Please respond with 'yes' or 'no' "\
                              "(or 'y' or 'n').\n")
 
-fnlist = sys.argv[1:]
+def get_parser():
+    parser = argparse.ArgumentParser(description="View input images and generate good/bad lists")
+    parser.add_argument('fn_list', nargs='+', help='Input filename(s)')
+    parser.add_argument('-prefix', type=str, default=None, help='Output prefix for good/bad lists')
+    return parser
 
-print
-print "Reviewing %i images" % len(fnlist)
-print
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
 
-good = []
-bad = []
+    fn_list = args.fn_list
 
-fig = plt.figure()
-ax  = fig.add_subplot(111)
-plt.ion()
-plt.show()
+    print
+    print "Reviewing %i images" % len(fn_list)
+    print
 
-#Use PIL Image
-basic = False 
+    good = []
+    bad = []
 
-for fn in fnlist:
-    print fn
-    plt.clf()
-    if basic:
-        im = mpimg.imread(fn) 
-        if im.ndim == 3:
-            cmap = None
-        plt.imshow(im, cmap=cmap)
-    else:
-        ds = gdal.Open(fn)
-        a = iolib.gdal_getma_sub(ds)
-        perc = malib.calcperc(a)
-        cmap = 'cpt_rainbow'
-        alpha = 1.0
-        if '_hs' in fn:
-            cmap = 'gray'
+    good_fn = "good_list.txt"
+    if args.prefix is not None:
+        good_fn = args.prefix+'_'+good_fn
+    good_f=open(good_fn, 'a', 0)
+
+    bad_fn = "bad_list.txt"
+    if args.prefix is not None:
+        bad_fn = args.prefix+'_'+bad_fn
+    bad_f=open(bad_fn, 'a', 0)
+
+    fig = plt.figure()
+    ax  = fig.add_subplot(111)
+    plt.ion()
+    plt.show()
+
+    #Use PIL Image
+    basic = False 
+
+    for fn in fn_list:
+        print fn
+        plt.clf()
+        if basic:
+            im = mpimg.imread(fn) 
+            if im.ndim == 3:
+                cmap = None
+            plt.imshow(im, cmap=cmap)
         else:
-            hs_fn = os.path.splitext(fn)[0]+'_hs.tif'
-            if os.path.exists(hs_fn):
-                hs_ds = gdal.Open(hs_fn)
-                hs = iolib.gdal_getma_sub(hs_ds)
-                hs_perc = malib.calcperc(hs)
-                plt.imshow(hs, cmap='gray', clim=hs_perc)
-                alpha = 0.5
-        plt.imshow(a, cmap=cmap, clim=perc, alpha=alpha)
-    fig.canvas.draw()
-    
-    if query_yes_no("{} good?".format(fn)):
-        good.append(fn)
-    else:
-        bad.append(fn)
+            ds = gdal.Open(fn)
+            a = iolib.gdal_getma_sub(ds)
+            perc = malib.calcperc(a)
+            cmap = 'cpt_rainbow'
+            alpha = 1.0
+            if '_hs' in fn:
+                cmap = 'gray'
+            else:
+                hs_fn = os.path.splitext(fn)[0]+'_hs.tif'
+                if os.path.exists(hs_fn):
+                    hs_ds = gdal.Open(hs_fn)
+                    hs = iolib.gdal_getma_sub(hs_ds)
+                    hs_perc = malib.calcperc(hs)
+                    plt.imshow(hs, cmap='gray', clim=hs_perc)
+                    alpha = 0.5
+            plt.imshow(a, cmap=cmap, clim=perc, alpha=alpha)
+        fig.canvas.draw()
+        
+        if query_yes_no("{} good?".format(fn)):
+            good.append(fn)
+            good_f.write("%s\n" % fn)
+        else:
+            bad.append(fn)
+            bad_f.write("%s\n" % fn)
 
-plt.close()
+    plt.close()
 
-print
-print "Good: %i" % (len(good))
-print good
-print
+    print
+    print "Good: %i" % (len(good))
+    print good
+    print
+    print "Bad: %i" % (len(bad))
+    print bad 
+    print
 
-good_fn = "good_list.txt"
-good_f=open(good_fn, 'a')
-for i in good:
-  good_f.write("%s\n" % i)
-good_f.close()
+    good_f.close()
+    bad_f.close()
 
-print "Bad: %i" % (len(bad))
-print bad 
-print
-
-bad_fn = "bad_list.txt"
-bad_f=open(bad_fn, 'a')
-for i in bad:
-  bad_f.write("%s\n" % i)
-bad_f.close()
-
+if __name__ == "__main__":
+    main()
