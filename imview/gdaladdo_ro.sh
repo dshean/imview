ffd70b40e1287439e14db153f8db50f98057d1c4
David Shean <dshean@gmail.com>
2017-07-17 13:39:39 -0700

    gdaladdo_ro: add parallel support

--- imview/gdaladdo_ro.sh
+++ imview/gdaladdo_ro.sh
@@ -5,4 +5,6 @@
 #r=gauss
 r=average
 
-gdaladdo -ro -r $r --config COMPRESS_OVERVIEW LZW --config BIGTIFF_OVERVIEW YES $1 2 4 8 16 32 64
+#gdaladdo -ro -r $r --config COMPRESS_OVERVIEW LZW --config BIGTIFF_OVERVIEW YES $1 2 4 8 16 32 64
+#Run in parallel for all inputs
+parallel --bar "gdaladdo -ro -r $r --config COMPRESS_OVERVIEW LZW --config BIGTIFF_OVERVIEW YES {} 2 4 8 16 32 64" ::: "$@" 
