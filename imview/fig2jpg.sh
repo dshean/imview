a250a981052c92631f92f0bbec5f9a88373308e6
David Shean <dshean@gmail.com>
2017-08-05 15:13:55 -0700

    fig2jpg: add notes

--- imview/fig2jpg.sh
+++ imview/fig2jpg.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+#This creates 150 dpi jpg images from inputs
+#Useful for preparing slides or documents where full-res versions just lead to filesize bloat
+
 #in=$1
 #out=${in%.*}_sm.jpg
 #convert $in -units PixelsPerInch -antialias -resize 1500x1500 -density 150 -quality 85 $out
