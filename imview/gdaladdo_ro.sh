f1706f541471182c696a3257c1a2b7ad1200d6b8
David Shean <dshean@gmail.com>
2017-11-28 10:53:29 -0800

    gdaladdo_ro: suppress progress bar for parallel

--- imview/gdaladdo_ro.sh
+++ imview/gdaladdo_ro.sh
@@ -6,5 +6,6 @@
 r=average
 
 #gdaladdo -ro -r $r --config COMPRESS_OVERVIEW LZW --config BIGTIFF_OVERVIEW YES $1 2 4 8 16 32 64
+
 #Run in parallel for all inputs
-parallel --bar "gdaladdo -ro -r $r --config COMPRESS_OVERVIEW LZW --config BIGTIFF_OVERVIEW YES {} 2 4 8 16 32 64" ::: "$@" 
+parallel --bar "gdaladdo -q -ro -r $r --config COMPRESS_OVERVIEW LZW --config BIGTIFF_OVERVIEW YES {} 2 4 8 16 32 64" ::: "$@" 
