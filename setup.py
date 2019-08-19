a85ad1c01e03f3816fbc7a633913be0382005ac2
Joachim Meyer <jomey@uw.edu>
2019-08-19 13:22:01 -0700

    Setup.py - Add `imviewer` as console script to package.

--- setup.py
+++ setup.py
@@ -2,22 +2,23 @@
 
 from distutils.core import setup
 
-#To prepare a new release
-#python setup.py sdist upload
+# To prepare a new release
+# python setup.py sdist upload
 
 setup(name='imview',
-    version='0.3.0',
-    description='Image viewers for geospatial data',
-    author='David Shean',
-    author_email='dshean@gmail.com',
-    license='MIT',
-    url='https://github.com/dshean/imview',
-    packages=['imview','imview.lib'],
-    long_description=open('README.md').read(),
-    install_requires=['numpy','gdal','matplotlib', 'pygeotools', 'matplotlib-scalebar', 'matplotlib-colorbar'],
-    #Note: this will write to /usr/local/bin
-    #scripts=['imview/imviewer.py','imview/iv.py','imview/stack_view.py', \
-    #'imview/review_img.py', 'imview/color_hs.py', \
-    #'imview/gdaladdo_ro.sh', 'imview/hs.sh', 'imview/fig2jpg.sh']
-)
-
+      version='0.3.0',
+      description='Image viewers for geospatial data',
+      author='David Shean',
+      author_email='dshean@gmail.com',
+      license='MIT',
+      url='https://github.com/dshean/imview',
+      packages=['imview', 'imview.lib'],
+      long_description=open('README.md').read(),
+      install_requires=['numpy', 'gdal', 'matplotlib', 'pygeotools',
+                        'matplotlib-scalebar', 'matplotlib-colorbar'],
+      entry_points={
+          'console_scripts': [
+              'imviewer = imview.imviewer:main',
+          ]
+      }
+      )
