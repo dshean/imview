9920ce24be86d4cbc040a2a6a805f9098cb33b6e
David Shean <dshean@gmail.com>
2020-12-23 20:28:28 -0800

    fig2jpg: add notes

--- imview/fig2jpg.sh
+++ imview/fig2jpg.sh
@@ -7,4 +7,12 @@
 #out=${in%.*}_sm.jpg
 #convert $in -units PixelsPerInch -antialias -resize 1500x1500 -density 150 -quality 85 $out
 
-parallel "convert {} -units PixelsPerInch -antialias -resize 1500x1500 -density 150 -quality 85 {.}_sm.jpg" ::: $@
+#This is 10" @ 150 dpi (pptx standard)
+#size=1500
+#This is 13.33" @ 150 dpi (pptx widescreen)
+#size=2000
+#parallel "convert {} -units PixelsPerInch -antialias -resize ${size}x${size} -density 150 -quality 85 {.}_sm.jpg" ::: $@
+
+#This is 10" @ 300 dpi
+size=3000
+parallel "convert {} -units PixelsPerInch -antialias -resize ${size}x${size} -density 300 -quality 95 {.}_300dpi.jpg" ::: $@
