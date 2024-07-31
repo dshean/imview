c33282f3e01b0002fa6dc469c2cad642edaae192
David Shean <dshean@gmail.com>
2024-07-30 17:15:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 #python setup.py sdist upload
 
 setup(name='imview',
-    version='0.3.0',
+    version='0.4.0',
     description='Image viewers for geospatial data',
     author='David Shean',
     author_email='dshean@gmail.com',
@@ -14,6 +14,7 @@ setup(name='imview',
     url='https://github.com/dshean/imview',
     packages=['imview','imview.lib'],
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=['numpy','gdal','matplotlib', 'pygeotools', 'matplotlib-scalebar', 'matplotlib-colorbar'],
     #Note: this will write to /usr/local/bin
     #scripts=['imview/imviewer.py','imview/iv.py','imview/stack_view.py', \
